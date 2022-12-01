391e204b7ca8d85d004c4e057c3ded97a9eaffce
Max Thomas <max.thomas@outlook.com>
2022-12-01 11:58:50 -0600

    Added explicity copy constructor and assignment to Card bc enum member variables are default copyable

--- Card.hpp
+++ Card.hpp
@@ -1,3 +1,5 @@
+#pragma once
+
 #include <iostream>
 #include <map>
 #include <string>
@@ -6,72 +8,84 @@
 
 class Card
 {
-    public:
-        enum class Suit { Spades, Clubs, Diamonds, Hearts, Joker };
-        enum class Rank { Ace, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King, Joker };
+public:
+    enum class Suit { Spades, Clubs, Diamonds, Hearts, Joker };
+    enum class Rank { Ace, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King, Joker };
 
-    private:
-        const Suit suit;
-        const Rank rank;
+private:
+    Suit suit;
+    Rank rank;
 
-    public:
-        Card(Suit _suit, Rank _rank):
-            suit(_suit),
-            rank(_rank)
+public:
+    Card(Suit _suit, Rank _rank):
+        suit(_suit),
+        rank(_rank)
+    {
+        if ((suit == Suit::Joker && rank != Rank::Joker) || (suit != Suit::Joker && rank == Rank::Joker))
+            throw std::invalid_argument("Joker Suit can only go with Joker Rank.");
+    }
+
+    Card(const Card& c):
+        suit(c.suit),
+        rank(c.rank)
+    {}
+
+    Card& operator=(const Card& c)
+    {
+        suit = c.suit;
+        rank = c.rank;
+        return *this;
+    }
+
+    inline bool operator==(const Card& rhs) const
+    {
+        return suit == rhs.suit && rank == rhs.rank;
+    }
+
+    inline bool operator!=(const Card& rhs) const
+    {
+        return !operator==(rhs);
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const Card& c)
+    {
+        if (c.rank == Rank::Joker)
+            return os << c.rank_str();
+        return os << c.rank_str() << " of " << c.suit_str();
+    }
+
+private:
+    const char* suit_str() const
+    {
+        switch (suit)
         {
-            if ((suit == Suit::Joker && rank != Rank::Joker) || (suit != Suit::Joker && rank == Rank::Joker))
-                throw std::invalid_argument("Joker Suit can only go with Joker Rank.");
+            case Suit::Spades: return "Spades";
+            case Suit::Clubs: return "Clubs";
+            case Suit::Diamonds: return "Diamonds";
+            case Suit::Hearts: return "Hearts";
+            default: return "";
         }
+    }
 
-        friend bool operator==(const Card& lhs, const Card& rhs)
+    const char* rank_str() const
+    {
+        switch (rank)
         {
-            return lhs.suit == rhs.suit && lhs.rank == rhs.rank;
+            case Rank::Ace: return "Ace";
+            case Rank::Two: return "Two";
+            case Rank::Three: return "Three";
+            case Rank::Four: return "Four";
+            case Rank::Five: return "Five";
+            case Rank::Six: return "Six";
+            case Rank::Seven: return "Seven";
+            case Rank::Eight: return "Eight";
+            case Rank::Nine: return "Nine";
+            case Rank::Ten: return "Ten";
+            case Rank::Jack: return "Jack";
+            case Rank::Queen: return "Queen";
+            case Rank::King: return "King";
+            case Rank::Joker: return "Joker";
+            default: return "";
         }
-
-        friend bool operator!=(const Card& lhs, const Card& rhs)
-        {
-            return !(lhs == rhs);
-        }
-
-        friend std::ostream& operator<<(std::ostream& os, const Card& c)
-        {
-            if (c.rank == Rank::Joker)
-                return os << c.rank_str();
-            return os << c.rank_str() << " of " << c.suit_str();
-        }
-
-    private:
-        const char* suit_str() const
-        {
-            switch (suit)
-            {
-                case Suit::Spades: return "Spades";
-                case Suit::Clubs: return "Clubs";
-                case Suit::Diamonds: return "Diamonds";
-                case Suit::Hearts: return "Hearts";
-                default: return "";
-            }
-        }
-
-        const char* rank_str() const
-        {
-            switch (rank)
-            {
-                case Rank::Ace: return "Ace";
-                case Rank::Two: return "Two";
-                case Rank::Three: return "Three";
-                case Rank::Four: return "Four";
-                case Rank::Five: return "Five";
-                case Rank::Six: return "Six";
-                case Rank::Seven: return "Seven";
-                case Rank::Eight: return "Eight";
-                case Rank::Nine: return "Nine";
-                case Rank::Ten: return "Ten";
-                case Rank::Jack: return "Jack";
-                case Rank::Queen: return "Queen";
-                case Rank::King: return "King";
-                case Rank::Joker: return "Joker";
-                default: return "";
-            }
-        }
+    }
 };
